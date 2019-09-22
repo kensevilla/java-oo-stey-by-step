a30e702c1f58bfcf5f784b442f94297271e920cd
Ken Sevilla <ken.michael.sevilla@oocl.com>
2019-09-22 23:15:05 +0800

    practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,37 @@
 package practice09;
 
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> studentList = new ArrayList<>();
+    public Klass(int number){
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName(){
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student student){
+        if(studentList.contains(student)){ this.leader = student;}
+        else{
+            System.setOut(System.out.append("It is not one of us.\n"));
+        }
+    }
+
+    public Student getLeader(){
+        return this.leader;
+    }
+
+    public void appendMember(Student student){
+        studentList.add(student);
+    }
 }
